3a6903dccc0ae30e6623c89f27807471ba3939a7
paigeman <53284808+paigeman@users.noreply.github.com>
2023-06-25 11:59:26 +0800

    Support method return instructions examples

---
+++ jvm-basic/src/main/java/org/fade/demo/jvm/basic/instructions/MethodReturnExample.java
@@ -0,0 +1,49 @@
+package org.fade.demo.jvm.basic.instructions;
+
+/**
+ * 方法返回指令示例
+ * @author fade
+ */
+public class MethodReturnExample {
+
+    public boolean booleanReturn() {
+        return false;
+    }
+
+    public byte byteReturn() {
+        return 1;
+    }
+
+    public char charReturn() {
+        return 'a';
+    }
+
+    public short shortReturn() {
+        return 1;
+    }
+
+    public int intReturn() {
+        return 1;
+    }
+
+    public long longReturn() {
+        return 1;
+    }
+
+    public float floatReturn() {
+        return 1.f;
+    }
+
+    public double doubleReturn() {
+        return 1.;
+    }
+
+    public String referenceReturn() {
+        return "hello";
+    }
+
+    public void voidReturn() {
+
+    }
+
+}
