235058270bad082fb42187cdd5348dd3a339e825
paigeman <53284808+paigeman@users.noreply.github.com>
2023-07-09 17:26:23 +0800

    Support constant pool in clz file

---
+++ jvm-basic/src/main/java/org/fade/demo/jvm/basic/clz/ConstantPoolExample.java
@@ -0,0 +1,17 @@
+package org.fade.demo.jvm.basic.clz;
+
+/**
+ * 常量池示例
+ * @author fade
+ */
+public class ConstantPoolExample {
+
+    private static String st = "abc";
+
+    private static final String fst = "abc";
+
+    public static void main(String[] args) {
+        String a = "abc";
+    }
+
+}
