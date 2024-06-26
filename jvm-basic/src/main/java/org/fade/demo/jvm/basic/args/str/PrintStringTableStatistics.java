1aab90399b96fa48e2ef9002c9b7e61b67989afe
paigeman <53284808+paigeman@users.noreply.github.com>
2024-06-26 21:58:14 +0800

    add some example about jvm option -XX:+PrintStringTableStatistics

---
+++ jvm-basic/src/main/java/org/fade/demo/jvm/basic/args/str/PrintStringTableStatistics.java
@@ -0,0 +1,12 @@
+package org.fade.demo.jvm.basic.args.str;
+
+/**
+ * @author fade
+ */
+public class PrintStringTableStatistics {
+
+    public static void main(String[] args) {
+        // -XX:+PrintStringTableStatistics
+    }
+
+}
